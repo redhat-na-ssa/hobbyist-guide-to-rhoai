863b060a07c1ff13cafe764ecca2c158b54f9fcd
HEMANG SHISHIR <hshishir@redhat.com>
2024-10-06 22:12:42 -0700

    add banner

--- scripts/runstep.sh
+++ scripts/runstep.sh
@@ -16,6 +16,7 @@ help() {
     loginfo "Options:"
     loginfo " -h, --help                  Show usage"
     loginfo " -s, --step                  step number (required)"
+    loginfo "                             0     - Install prerequisites"
     loginfo "                             1     - Add administrative user"
     loginfo "                             2     - (Optional) Install web terminal"
     loginfo "                             3     - Install kserve dependencies"
@@ -55,6 +56,11 @@ create_log_file() {
     touch logs/"${LOG_FILE}"
 }
 
+step_0() {
+    logbanner "Install prerequisites"
+    until oc apply -f "${PARENT_DIR}"/configs/00; do : ; done
+}
+
 step_1() {
     logbanner "Adding administrative user"
     USER="admin1"
@@ -65,17 +71,17 @@ step_1() {
 step_2() {
     logbanner "(Optional) Install web terminal"
     loginfo "Web Terminal"
-    until oc apply -f "${PROJECT_DIR}"/configs/02; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/02; do : ; done
 }
 
 step_3() {
     logbanner "Install kserve dependencies"
-    until oc apply -f "${PROJECT_DIR}"/configs/03; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/03; do : ; done
 }
 
 step_4() {
     logbanner "Install RHOAI operator"
-    until oc apply -f "${PROJECT_DIR}"/configs/04; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/04; do : ; done
 }
 
 step_5() {
@@ -93,37 +99,37 @@ step_7() {
     loginfo "Create a GPU node with autoscaling"
     ocp_aws_cluster_autoscaling
     ocp_scale_machineset
-    until oc apply -f "${PROJECT_DIR}"/configs/07; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/07; do : ; done
 }
 
 step_8() {
     logbanner "Run sample gpu application"
-    until oc apply -f "${PROJECT_DIR}"/configs/08; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/08; do : ; done
 }
 
 step_9() {
     logbanner "Configure gpu dashboards"
-    until oc apply -f "${PROJECT_DIR}"/configs/09; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/09; do : ; done
 }
 
 step_10() {
     logbanner "Configure gpu sharing method"
-    until oc apply -f "${PROJECT_DIR}"/configs/10; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/10; do : ; done
 }
 
 step_11() {
     logbanner "Configure distributed workloads"
-    until oc apply -f "${PROJECT_DIR}"/configs/11; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/11; do : ; done
 }
 
 step_12() {
     logbanner "Configure codeflare operator"
-    until oc apply -f "${PROJECT_DIR}"/configs/12; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/12; do : ; done
 }
 
 step_13() {
     logbanner "Configure rhoai"
-    until oc apply -f "${PROJECT_DIR}"/configs/13; do : ; done
+    until oc apply -f "${PARENT_DIR}"/configs/13; do : ; done
 }
 
 setup(){
@@ -133,7 +139,13 @@ setup(){
         help
     fi
 
-    create_log_file
+    # create_log_file
+
+    if [ "$s" -eq 0 ] ; then
+        loginfo "Running step 0"
+        step_0
+        exit 0
+    fi
 
     for (( i=1; i <= s; i++ ))
     do
